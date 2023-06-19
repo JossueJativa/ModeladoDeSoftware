34e6285078482b65cc6431bf54cc0f48384771e0
Jossue Jativa <jativatamayojossuedavid@gmail.com>
2023-06-18 22:50:47 -0500

    Borradas las cossas que no funcionan

--- RecursosHumanos/CentroCostos/models.py
+++ RecursosHumanos/CentroCostos/models.py
@@ -1,9 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class usuarios(models.Model):
-    username = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
-
-    def __str__ (self):
-        return f"{self.username} {self.password}"
